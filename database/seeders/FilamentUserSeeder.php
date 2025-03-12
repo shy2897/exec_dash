8780a1b85f2c4b02ec3da1b5ce3ad6658252460b
2023080179 <2023080179@bank.bnb>
2025-03-12 16:07:44 +0600

    Seeder Changes 1

--- database/seeders/FilamentUserSeeder.php
+++ database/seeders/FilamentUserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class FilamentUserSeeder extends Seeder
 {
